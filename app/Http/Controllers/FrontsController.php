ff80352809eae5b9857b42955f9943cef6796139
mdsharafat <mdsharafat047@gmail.com>
2019-03-18 09:54:16 +0600

    ClaimsController added all claim from shifted by sharafat

--- app/Http/Controllers/FrontsController.php
+++ app/Http/Controllers/FrontsController.php
@@ -16,39 +16,4 @@ class FrontsController extends Controller
       return view('frontEnd.login');
   }
 
-  public function claim()
-  {
-      return view('frontEnd.claim.claim');
-  }
-
-  public function missed_connection()
-  {
-      return view('frontEnd.claim.missed_connection');
-  }
-
-  public function flight_delay()
-  {
-      return view('frontEnd.claim.flight_delay');
-  }
-
-  public function flight_cancellation()
-  {
-      return view('frontEnd.claim.flight_cancellation');
-  }
-
-  public function delay_luggage()
-  {
-      return view('frontEnd.claim.delay_luggage');
-  }
-
-  public function lost_luggage()
-  {
-      return view('frontEnd.claim.lost_luggage');
-  }
-
-  public function denied_boarding()
-  {
-      return view('frontEnd.claim.denied_boarding');
-  }
-
 }
--- routes/web.php
+++ routes/web.php
@@ -19,13 +19,13 @@ Route::get('user/signup','FrontsController@user_signup')->name('user/signup');
 Route::get('/user/login','FrontsController@user_login')->name('user/login');
 
 // claim
-Route::get('/claim','FrontsController@claim')->name('claim');
-Route::get('/missed-connection','FrontsController@missed_connection')->name('missed-connection');
-Route::get('/flight-delay','FrontsController@flight_delay')->name('flight-delay');
-Route::get('/flight-cancellation','FrontsController@flight_cancellation')->name('flight-cancellation');
-Route::get('/delay-luggage','FrontsController@delay_luggage')->name('delay-luggage');
-Route::get('/lost-luggage','FrontsController@lost_luggage')->name('lost-luggage');
-Route::get('/denied-boarding','FrontsController@denied_boarding')->name('denied-boarding');
+Route::get('/claim','ClaimsController@claim')->name('claim');
+Route::get('/missed-connection','ClaimsController@missed_connection')->name('missed-connection');
+Route::get('/flight-delay','ClaimsController@flight_delay')->name('flight-delay');
+Route::get('/flight-cancellation','ClaimsController@flight_cancellation')->name('flight-cancellation');
+Route::get('/delay-luggage','ClaimsController@delay_luggage')->name('delay-luggage');
+Route::get('/lost-luggage','ClaimsController@lost_luggage')->name('lost-luggage');
+Route::get('/denied-boarding','ClaimsController@denied_boarding')->name('denied-boarding');
 
 /* ADMIN ROUTE */
 
