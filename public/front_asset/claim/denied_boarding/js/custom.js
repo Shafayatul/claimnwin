8ce8df2d868f0123c0c9c8b337845f92e460efa0
Shafayatul <aabir68@yahoo.com>
2019-03-27 17:49:10 +0600

    denied-boarding front end done

--- public/front_asset/claim/denied_boarding/js/custom.js
+++ public/front_asset/claim/denied_boarding/js/custom.js
@@ -1,9 +1,91 @@
 $(document).ready(function() {
 
-    var keyCount = 0;
-    $("#add_connection").click(function() {
-        $("<div class='child_div' style='margin-top: 10px;' id='property_remove_" + keyCount + "'><input  style='width: 75%; float: left; margin-right: 10px; margin-bottom: 0px; margin-top: 0px;' type='text' class='common_input connection' id='connection' name='connection[]'/> <button type='button' class='remove_property' id='" + keyCount + "' style='float: none;margin-left: 5px;margin-top: 2px;'><i class='fas fa-minus-circle'></i></button></div>").appendTo("#property");
+    /*HIDE MOBILE TABLE*/
+    if($(window).width() > 768){
+      $('.table_show_on_small_screen').remove();
+    }else{
+      $('.show_on_is_obtained_full_reimbursement_no').remove();
+    }
+
+
+    $(document).on('click', '#continue_7', function(){
+        $("#step-form").submit();
     });
+
+    var keyCount = 0;
+    $("#add_connection").click(function(){
+      $("<div class='child_div' style='margin-top: 10px;' id='property_remove_"+keyCount+"'><input  style='width: 75%; float: left; margin-right: 10px; margin-bottom: 0px; margin-top: 0px;' type='text' class='auto_airport_complete common_input connection' id='connection' name='connection[]'/> <button type='button' class='remove_property' id='"+keyCount+"' style='float: none;margin-left: 5px;margin-top: 2px;'><i class='fas fa-minus-circle'></i></button></div>").appendTo("#property");
+      auto_airport_complete();
+    });
+
+
+    /**
+    * flight list dynamic checkbox
+    */
+    $(document).on('change', "input[name='departed_from'], input[name='final_destination'], input[name='is_direct_flight'], .connection", function(){
+       if ($("input[name='is_direct_flight']:checked").val() == '1') {
+          var is_connection_empty = true;
+          $(".connection").each(function(){
+            if ($(this).val() != "") {
+              is_connection_empty = false;
+            }
+          });
+          if (!is_connection_empty) {
+            itinerary_details_for_your_disrupted_flight_html('multiple');
+          }
+        }else{
+          itinerary_details_for_your_disrupted_flight_html('single');
+        }
+    });
+
+
+    $(document).on('change', '.airline', function(){
+      console.log('working');
+      var iata_code = $(this).attr('iata_code');
+      var serial = $(this).attr('serial');
+      console.log(iata_code);
+      console.log(serial);
+      console.log($(".flight_code_"+serial).val(iata_code));
+    });
+
+    function itinerary_details_for_your_disrupted_flight_html(type){
+
+      if (type=='single') {
+        var value = $("input[name='departed_from']").attr('iata-code')+'-'+$("input[name='final_destination']").attr('iata-code');
+        var html = '<div class="common_row"><div class="parent_div"><div class="single_child_div"><div class="arrival_to_destination_text_div"><span class="arrival_to_destination_text_span">'+$("input[name='departed_from']").val()+'<i class="fas fa-plane"></i>'+$("input[name='final_destination']").val()+'</span></div></div></div><div class="parent_div"><div class="single_child_div"><div class="left_div"><div class="label_field"><label for="airline">AIRLINE</label></div><div class="input_field"><input type="text" serial="1" class="auto_airline_complete common_input airline" id="common_input airline" name="airline[]" placeholder="e.g. New York or JFK"><input type="hidden" name="flight_segment[]" value="'+value+'"></div></div><div class="right_div"><div class="flight_number_div"><div class="label_field"><label for="departure_airport">FLIGHT NO.</label></div><div class="two_input_field"><div class="child_two_input_field_left"><div class="input_field"><input type="text" class="common_input flight_code flight_code_1" id="common_input flight_code" name="flight_code[]" placeholder=""></div></div><div class="child_two_input_field_right"><div class="input_field"><input type="text" class="common_input flight_number" id="common_input flight_number" name="flight_number[]" placeholder="1234"></div></div></div></div><div class="departure_date_div"><div class="label_field"><label for="departure_airport">DEPARTURE DATE</label></div><div class="two_input_field"><div class="input_field"><input type="text" class="common_input departure_airport date" id="common_input departure_airport date" name="departure_date[]" placeholder="e.g. New York or JFK"></div></div></div></div></div></div></div>';
+        $('.itinerary_flight_element').html(html);
+
+      }else if (type=='multiple') {
+
+        var airport_array_temp = new Array();
+        var airport_array_iata_code_temp = new Array();
+
+        airport_array_temp.push($("input[name='departed_from']").val());
+        airport_array_iata_code_temp.push($("input[name='departed_from']").attr('iata-code'));
+
+        $(".connection").each(function(){
+          if ($(this).val() != "") {
+            airport_array_temp.push($(this).val());
+            airport_array_iata_code_temp.push($(this).attr('iata-code'));
+          }
+        });
+        airport_array_temp.push($("input[name='final_destination']").val());
+        airport_array_iata_code_temp.push($("input[name='final_destination']").attr('iata-code'));
+
+        var html='';
+        for (var i = 0; i < airport_array_temp.length-1; i++) {
+          j=i+1;
+          var value = airport_array_iata_code_temp[i]+'-'+airport_array_iata_code_temp[j];
+          html += '<div class="common_row"><div class="parent_div"><div class="single_child_div"><div class="arrival_to_destination_text_div"><span class="arrival_to_destination_text_span">'+airport_array_temp[i]+'<i class="fas fa-plane"></i>'+airport_array_temp[j]+'</span></div></div></div><div class="parent_div"><div class="single_child_div"><div class="left_div"><div class="label_field"><label for="airline">AIRLINE</label></div><div class="input_field"><input type="text" serial="'+j+'" class="auto_airline_complete common_input airline" id="common_input airline" name="airline[]" placeholder="e.g. New York or JFK"><input type="hidden" name="flight_segment[]" value="'+value+'"></div></div><div class="right_div"><div class="flight_number_div"><div class="label_field"><label for="departure_airport">FLIGHT NO.</label></div><div class="two_input_field"><div class="child_two_input_field_left"><div class="input_field"><input type="text" class="common_input flight_code flight_code_'+j+'" id="common_input flight_code" name="flight_code[]" placeholder=""></div></div><div class="child_two_input_field_right"><div class="input_field"><input type="text" class="common_input flight_number" id="common_input flight_number" name="flight_number[]" placeholder="1234"></div></div></div></div><div class="departure_date_div"><div class="label_field"><label for="departure_airport">DEPARTURE DATE</label></div><div class="two_input_field"><div class="input_field"><input type="text" class="common_input departure_airport date" id="common_input departure_airport date" name="departure_date[]" placeholder="e.g. New York or JFK"></div></div></div></div></div></div></div>';
+        }
+        $('.itinerary_flight_element').html(html);
+      }
+
+      date_picker();
+      auto_airline_complete();
+
+    }
+
 
     $(document).on('click', '.remove_property', function() {
         var id = $(this).attr('id');
@@ -70,9 +152,15 @@ $(document).ready(function() {
             }
         } else if (step == 3) {
             $("#continue_3").removeClass('active_button');
+            // special necessary email
+            if ($("input[name='email_address']").val() == "")
+            {
+              return false;
+            }
+
+
             var spend_on_accommodation = false;
             var expenses_table = false;
-            var email_address = false;
             if (($("input[name='is_spend_on_accomodation']").is(':checked'))) {
                 spend_on_accommodation = true;
             } else {
@@ -83,16 +171,11 @@ $(document).ready(function() {
             } else {
                 expenses_table = false;
             }
-            if ($("input[name='email_address']").val() != "") {
-                console.log($("input[name='email_address']").val());
-                email_address = true;
-            } else {
-                email_address = false;
-            }
+
             if (($("input[name='is_rerouted']").is(':checked'))) {
                 if ($('input[name=is_rerouted]:checked').val() == '1') {
                     console.log($('input[name=is_rerouted]:checked').val());
-                    if (spend_on_accommodation == true && expenses_table == true && email_address == true) {
+                    if (spend_on_accommodation == true && expenses_table == true) {
                         $("#continue_3").addClass('active_button');
                         $("#continue_4").addClass('active_button');
                         return true;
@@ -102,7 +185,7 @@ $(document).ready(function() {
                 } else if ($('input[name=is_rerouted]:checked').val() == '0') {
                     if (($("input[name='is_obtained_full_reimbursement']").is(':checked'))) {
                         if ($('input[name=is_obtained_full_reimbursement]:checked').val() == '1') {
-                            if (spend_on_accommodation == true && expenses_table == true && email_address == true) {
+                            if (spend_on_accommodation == true && expenses_table == true) {
                                 $("#continue_3").addClass('active_button');
                                 $("#continue_4").addClass('active_button');
                                 return true;
@@ -118,7 +201,7 @@ $(document).ready(function() {
                                         return true;
                                     }
                                 } else if ($('input[name=is_paid_for_rerouting]:checked').val() == '0') {
-                                    if (spend_on_accommodation == true && expenses_table == true && email_address == true) {
+                                    if (spend_on_accommodation == true && expenses_table == true) {
                                         $("#continue_3").addClass('active_button');
                                         $("#continue_4").addClass('active_button');
                                         return true;
@@ -451,13 +534,17 @@ $(document).ready(function() {
     fileInput3.addEventListener("change", function(event) {
         the_return3.innerHTML = this.value;
     });
-
+  date_picker();
+  function date_picker(){
     var date_input = $('.date'); //our date input has the name "date"
     var options = {
-        format: 'mm/dd/yyyy',
-        endDate: '+0d',
-        todayHighlight: true,
-        autoclose: true,
+      format: 'mm/dd/yyyy',
+      endDate: '+0d',
+      todayHighlight: true,
+      autoclose: true,
     };
-    date_input.datepicker(options);
+    date_input.datepicker(options);  
+  }
+
+
 });
