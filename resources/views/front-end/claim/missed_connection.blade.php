ebc6111a5c3aa2f25853399489db9f2812b5e5a8
Shafayatul <aabir68@yahoo.com>
2019-04-02 16:03:28 +0600

    signature pad front end done

--- resources/views/front-end/claim/missed_connection.blade.php
+++ resources/views/front-end/claim/missed_connection.blade.php
@@ -5,8 +5,10 @@
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/css/bootstrap-datepicker3.css" />
   {{-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> --}}
-<link rel="stylesheet" type="text/css" href="{{ asset('front_asset/') }}/claim/missed_connection/css/main.css">
+<link rel="stylesheet" type="text/css" href="{{asset('front_asset/') }}/claim/missed_connection/css/main.css">
 <!--===============================================================================================-->
+
+
 @endsection
 
 @section('content')
@@ -19,6 +21,7 @@
         <!-- ...................................................................
                                   STEP 1 STARTS
         .................................................................... -->
+
         <div class="single_step" id="step_1">
           <div class="common_row">
             <div class="form_h3">
@@ -1019,10 +1022,28 @@
                   <input type="checkbox" name="is_signed_permission"> <span class="label-text">Write your signature below as it appears on your ID. It's required by airlines to collect the compensation for you. By signing you agree with the Assignment Form and Price List</span>
                 </label>
               </div>
-              {{-- <div class="custom-control custom-checkbox">
-                <input type="checkbox" class="custom-control-input" id="customCheck1">
-                <label class="custom-control-label" for="customCheck1">Write your signature below as it appears on your ID. It's required by airlines to collect the compensation for you. By signing you agree with the Assignment Form and Price List</label>
-              </div> --}}
+              <div class="container">
+                <div class="row">
+                  <div class="col-md-offset-1 col-md-10">
+                    <div class="panel panel-default">
+                      <div class="panel-heading">Digital Signature:</div>
+                      <div class="panel-body center-text">
+
+                        <div id="signArea" >
+                          <h2 class="tag-ingo">Put signature below,</h2>
+                          <div class="sig sigWrapper" style="height:auto; width:302px">
+                            <div class="typed"></div>
+                            <canvas class="sign-pad" id="sign-pad" width="300" height="100"></canvas>
+                          </div>
+                        </div>
+
+                        <button id="btnSaveSign">Save Signature</button>
+
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
 
@@ -1254,6 +1275,17 @@
   </script>
 
 
+{{-- signature --}}
+  <link  href="{{asset('front_asset/signature/css/jquery.signaturepad.css')}}" rel="stylesheet">
+  <script src="{{asset('front_asset/signature/js/numeric-1.2.6.min.js')}}"></script> 
+  <script src="{{asset('front_asset/signature/js/bezier.js')}}"></script>
+  <script src="{{asset('front_asset/signature/js/jquery.signaturepad.js')}}"></script> 
+  
+  <script type='text/javascript' src="{{asset('front_asset/signature/js/html2canvas.js')}}"></script>
+  <script src="{{asset('front_asset/signature/js/json2.min.js')}}"></script>
+{{-- signature ends --}}
+
+
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/js/bootstrap-datepicker.min.js"></script>
@@ -1262,4 +1294,33 @@
 
 
 
+  <script>
+
+  $(document).ready(function(e){
+
+    $(document).ready(function() {
+      $('#signArea').signaturePad({drawOnly:true, drawBezierCurves:true, lineTop:90});
+    });
+    
+    $("#btnSaveSign").click(function(e){
+      html2canvas([document.getElementById('sign-pad')], {
+        onrendered: function (canvas) {
+          var canvas_img_data = canvas.toDataURL('image/png');
+          var img_data = canvas_img_data.replace(/^data:image\/(png|jpg);base64,/, "");
+          //ajax call to save image inside folder
+          $.ajax({
+            url: 'save_sign.php',
+            data: { img_data:img_data },
+            type: 'post',
+            dataType: 'json',
+            success: function (response) {
+               // window.location.reload();
+            }
+          });
+        }
+      });
+    });
+
+  });
+  </script>
 @endsection
