4c02476fd72472ed0493f37c83fad77cfd29e843
mdsharafat <mdsharafat047@gmail.com>
2019-04-08 12:00:18 +0600

    homepage header part completed by sharafat

--- resources/views/layouts/home/home.blade.php
+++ resources/views/layouts/home/home.blade.php
@@ -2,10 +2,132 @@
 
 @section('header-script')
 <link  href="{{asset('front_asset/front_pages_asset/css/home.css')}}" rel="stylesheet">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 @endsection
 
 @section('page-title')
-  <div class="page_title">
-    <h1 class="text-center">About Us</h1>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-7">
+        <div class="home_top_header_div">
+          <div class="row">
+            <div class="col-md-12" style="margin: 0px; padding: 0px;">
+              <div class="home_top_header_title_h1_div">
+                <h1>When travel goes wrong, <span class="extra_color">we </br>make it right.</span></h1>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-9" style="margin: 0px; padding: 0px;">
+              <div class="home_top_header_title_p_div">
+                <p>Travel disruptions happen, but that doesn't mean you have to accept them.</p>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-12" style="margin: 0px; padding: 0px;">
+              <div class="home_top_header_title_ul_div">
+                <ul class="fa-ul">
+                  <li><i class="fas fa-check-circle"></i>Get up to €1300 for a cancelled, overbooked or delayed flight.</li>
+                  <li><i class="fas fa-check-circle"></i>Get up to $3400 for a luggage issue.</li>
+                  <li><i class="fas fa-check-circle"></i>Get up to $7000 for expenses you incurred as a result of your distrupted flight.</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-12">
+              <div class="home_top_header_departed_destination_div">
+                <form class="" action="index.html" method="post">
+                  <div class="row">
+                    <div class="col-md-4" style="margin: 0px; padding: 0px;">
+                      <input class="common_input no_right_border" type="text" name="" value="" placeholder="Departed From">
+                    </div>
+                    <div class="col-md-4" style="margin: 0px; padding: 0px;">
+                      <input class="common_input" type="text" name="" value="" placeholder="Final Destination">
+                    </div>
+                    <div class="col-md-4" style="margin: 0px; padding: 0px;">
+                      <button class="common_button" type="button" name="button">CHECK COMPENSATION</button>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-12" style="margin: 0px; padding: 0px;">
+              <div class="home_top_header_options_div">
+                <div class="row">
+                  <div class="col-md-3">
+                    <div class="row">
+                      <div class="col-md-3" style="padding: 0px;">
+                        <div class="home_top_header_option_icon_div">
+                          <img src="{{ asset('/front_asset/front_pages_asset/img/homepage_top_header_free_icon.png') }}" alt="">
+                        </div>
+                      </div>
+                      <div class="col-md-9" style="margin: 0px; padding: 0px;">
+                        <div class="home_top_header_option_text_div text-center">
+                          <p class="home_top_header_option_text_p_title_div">It's FREE</p>
+                          <p class="home_top_header_option_text_p_normal_div">to claim</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-md-3">
+                    <div class="row">
+                      <div class="col-md-3" style="padding: 0px;">
+                        <div class="home_top_header_option_icon_div">
+                          <img src="{{ asset('/front_asset/front_pages_asset/img/homepage_top_header_dollar_icon.png') }}" alt="">
+                        </div>
+                      </div>
+                      <div class="col-md-9" style="margin: 0px; padding: 0px;">
+                        <div class="home_top_header_option_text_div text-center">
+                          <p class="home_top_header_option_text_p_title_div">Up to €600</p>
+                          <p class="home_top_header_option_text_p_normal_div">to claim</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-md-3">
+                    <div class="row">
+                      <div class="col-md-3" style="padding: 0px;">
+                        <div class="home_top_header_option_icon_div">
+                          <img src="{{ asset('/front_asset/front_pages_asset/img/homepage_top_header_group_icon.png') }}" alt="">
+                        </div>
+                      </div>
+                      <div class="col-md-9" style="margin: 0px; padding: 0px;">
+                        <div class="home_top_header_option_text_div text-center">
+                          <p class="home_top_header_option_text_p_title_div">Trusted</p>
+                          <p class="home_top_header_option_text_p_normal_div">to claim</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-md-3">
+                    <div class="row">
+                      <div class="col-md-3" style="padding: 0px;">
+                        <div class="home_top_header_option_icon_div">
+                          <img src="{{ asset('/front_asset/front_pages_asset/img/homepage_top_header_bank_icon.png') }}" alt="">
+                        </div>
+                      </div>
+                      <div class="col-md-9" style="margin: 0px; padding: 0px;">
+                        <div class="home_top_header_option_text_div text-center">
+                          <p class="home_top_header_option_text_p_title_div">8 Billion EUR</p>
+                          <p class="home_top_header_option_text_p_normal_div">to claim</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
+@endsection
+
+@section('content')
+
 @endsection
