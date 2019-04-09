4a9204d03c853b5c75984801d70b004308553692
mdsharafat <mdsharafat047@gmail.com>
2019-04-09 12:17:26 +0600

    homepage done without responsive by sharafat

--- resources/views/layouts/home/home.blade.php
+++ resources/views/layouts/home/home.blade.php
@@ -1,8 +1,11 @@
 @extends('layouts.front_layout')
 
 @section('header-script')
-<link  href="{{asset('front_asset/front_pages_asset/css/home.css')}}" rel="stylesheet">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+<link  href="{{asset('front_asset/front_pages_asset/css/slick.css')}}" rel="stylesheet">
+<link  href="{{asset('front_asset/front_pages_asset/css/slick-theme.css')}}" rel="stylesheet">
+<link  href="{{asset('front_asset/front_pages_asset/css/home.css')}}" rel="stylesheet">
+
 @endsection
 
 @section('page-title')
@@ -83,7 +86,7 @@
                       <div class="col-md-9" style="margin: 0px; padding: 0px;">
                         <div class="home_top_header_option_text_div text-center">
                           <p class="home_top_header_option_text_p_title_div">Up to €600</p>
-                          <p class="home_top_header_option_text_p_normal_div">to claim</p>
+                          <p class="home_top_header_option_text_p_normal_div">compensation</p>
                         </div>
                       </div>
                     </div>
@@ -98,7 +101,7 @@
                       <div class="col-md-9" style="margin: 0px; padding: 0px;">
                         <div class="home_top_header_option_text_div text-center">
                           <p class="home_top_header_option_text_p_title_div">Trusted</p>
-                          <p class="home_top_header_option_text_p_normal_div">to claim</p>
+                          <p class="home_top_header_option_text_p_normal_div">by millions</p>
                         </div>
                       </div>
                     </div>
@@ -113,7 +116,7 @@
                       <div class="col-md-9" style="margin: 0px; padding: 0px;">
                         <div class="home_top_header_option_text_div text-center">
                           <p class="home_top_header_option_text_p_title_div">8 Billion EUR</p>
-                          <p class="home_top_header_option_text_p_normal_div">to claim</p>
+                          <p class="home_top_header_option_text_p_normal_div">available to claim</p>
                         </div>
                       </div>
                     </div>
@@ -130,6 +133,7 @@
 
 @section('content')
 <div class="container">
+  <!-- How it works div starts -->
   <div class="how_it_works_div">
     <div class="row">
       <div class="col-md-12">
@@ -140,8 +144,8 @@
             </div>
           </div>
         </div>
-        <div class="row">
-          <div class="how_it_works_lower_div">
+        <div class="how_it_works_lower_div">
+          <div class="row">
             <div class="col-md-4 text-center">
               <div class="how_it_works_background_icon_div">
                 <div class="how_it_works_icon_div">
@@ -181,4 +185,202 @@
     </div>
   </div>
 </div>
+  <!-- How it works div ends -->
+
+  <!-- What customer say div starts -->
+
+  <div class="what_customer_say_div">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="what_customer_say_title_div text-center">
+            <h1>WHAT OUR CUSTOMERS SAY</h1>
+          </div>
+        </div>
+      </div>
+      <div class="what_customer_say_lower_div">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="slider-area slider">
+              <div class="sliding_div">
+                <div class="sliding_div_rating_div">
+                  <span class="rating_span"> <img src="{{ asset('/front_asset/front_pages_asset/img/rating_img.png') }}" alt=""> </span>
+                  <span class="rating_span"> <img src="{{ asset('/front_asset/front_pages_asset/img/rating_img.png') }}" alt=""> </span>
+                  <span class="rating_span"> <img src="{{ asset('/front_asset/front_pages_asset/img/rating_img.png') }}" alt=""> </span>
+                  <span class="rating_span"> <img src="{{ asset('/front_asset/front_pages_asset/img/rating_img.png') }}" alt=""> </span>
+                  <span class="rating_span"> <img src="{{ asset('/front_asset/front_pages_asset/img/rating_img.png') }}" alt=""> </span>
+                </div>
+                <div class="sliding_div_title_p_div">
+                  <p>Great advisor and transparent...</p>
+                </div>
+                <div class="sliding_div_text_p_div">
+                  <p>Great advisor and transparent communication</br> about the process. Fast turn around too.</br> Strongly recomend it</p>
+                </div>
+                <div class="sliding_div_customer_name_div">
+                  <p>Denise Roberts</p>
+                </div>
+              </div>
+              <div class="sliding_div">
+                <div class="sliding_div_rating_div">
+                  <span class="rating_span"> <img src="{{ asset('/front_asset/front_pages_asset/img/rating_img.png') }}" alt=""> </span>
+                  <span class="rating_span"> <img src="{{ asset('/front_asset/front_pages_asset/img/rating_img.png') }}" alt=""> </span>
+                  <span class="rating_span"> <img src="{{ asset('/front_asset/front_pages_asset/img/rating_img.png') }}" alt=""> </span>
+                  <span class="rating_span"> <img src="{{ asset('/front_asset/front_pages_asset/img/rating_img.png') }}" alt=""> </span>
+                  <span class="rating_span"> <img src="{{ asset('/front_asset/front_pages_asset/img/rating_img.png') }}" alt=""> </span>
+                </div>
+                <div class="sliding_div_title_p_div">
+                  <p>Great advisor and transparent...</p>
+                </div>
+                <div class="sliding_div_text_p_div">
+                  <p>Great advisor and transparent communication</br> about the process. Fast turn around too.</br> Strongly recomend it</p>
+                </div>
+                <div class="sliding_div_customer_name_div">
+                  <p>Clare Burchell</p>
+                </div>
+              </div>
+              <div class="sliding_div">
+                <div class="sliding_div_rating_div">
+                  <span class="rating_span"> <img src="{{ asset('/front_asset/front_pages_asset/img/rating_img.png') }}" alt=""> </span>
+                  <span class="rating_span"> <img src="{{ asset('/front_asset/front_pages_asset/img/rating_img.png') }}" alt=""> </span>
+                  <span class="rating_span"> <img src="{{ asset('/front_asset/front_pages_asset/img/rating_img.png') }}" alt=""> </span>
+                  <span class="rating_span"> <img src="{{ asset('/front_asset/front_pages_asset/img/rating_img.png') }}" alt=""> </span>
+                  <span class="rating_span"> <img src="{{ asset('/front_asset/front_pages_asset/img/rating_img.png') }}" alt=""> </span>
+                </div>
+                <div class="sliding_div_title_p_div">
+                  <p>Great advisor and transparent...</p>
+                </div>
+                <div class="sliding_div_text_p_div">
+                  <p>Great advisor and transparent communication</br> about the process. Fast turn around too.</br> Strongly recomend it</p>
+                </div>
+                <div class="sliding_div_customer_name_div">
+                  <p>Kjell Caminha</p>
+                </div>
+              </div>
+              <div class="sliding_div">
+                <div class="sliding_div_rating_div">
+                  <span class="rating_span"> <img src="{{ asset('/front_asset/front_pages_asset/img/rating_img.png') }}" alt=""> </span>
+                  <span class="rating_span"> <img src="{{ asset('/front_asset/front_pages_asset/img/rating_img.png') }}" alt=""> </span>
+                  <span class="rating_span"> <img src="{{ asset('/front_asset/front_pages_asset/img/rating_img.png') }}" alt=""> </span>
+                  <span class="rating_span"> <img src="{{ asset('/front_asset/front_pages_asset/img/rating_img.png') }}" alt=""> </span>
+                  <span class="rating_span"> <img src="{{ asset('/front_asset/front_pages_asset/img/rating_img.png') }}" alt=""> </span>
+                </div>
+                <div class="sliding_div_title_p_div">
+                  <p>Great advisor and transparent...</p>
+                </div>
+                <div class="sliding_div_text_p_div">
+                  <p>Great advisor and transparent communication</br> about the process. Fast turn around too.</br> Strongly recomend it</p>
+                </div>
+                <div class="sliding_div_customer_name_div">
+                  <p>Sarah Green</p>
+                </div>
+              </div>
+              <div class="sliding_div">
+                <div class="sliding_div_rating_div">
+                  <span class="rating_span"> <img src="{{ asset('/front_asset/front_pages_asset/img/rating_img.png') }}" alt=""> </span>
+                  <span class="rating_span"> <img src="{{ asset('/front_asset/front_pages_asset/img/rating_img.png') }}" alt=""> </span>
+                  <span class="rating_span"> <img src="{{ asset('/front_asset/front_pages_asset/img/rating_img.png') }}" alt=""> </span>
+                  <span class="rating_span"> <img src="{{ asset('/front_asset/front_pages_asset/img/rating_img.png') }}" alt=""> </span>
+                  <span class="rating_span"> <img src="{{ asset('/front_asset/front_pages_asset/img/rating_img.png') }}" alt=""> </span>
+                </div>
+                <div class="sliding_div_title_p_div">
+                  <p>Great advisor and transparent...</p>
+                </div>
+                <div class="sliding_div_text_p_div">
+                  <p>Great advisor and transparent communication</br> about the process. Fast turn around too.</br> Strongly recomend it</p>
+                </div>
+                <div class="sliding_div_customer_name_div">
+                  <p>Denise Roberts</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- What customer say div ends -->
+
+  <!-- Our Process div Starts -->
+  <div class="our_process_div">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="our_process_div_title_div text-center">
+            <h1>OUR PROCESS</h1>
+          </div>
+        </div>
+      </div>
+      <div class="our_process_lower_div">
+        <div class="row">
+          <div class="col-md-3">
+            <div class="our_process_lower_div_icon_div text-center">
+              <img class="" src="{{ asset('/front_asset/front_pages_asset/img/homepage_our_process_calculator.png') }}" alt="">
+            </div>
+            <div class="our_process_lower_div_icon_text_div text-center">
+              <p>Check your flight with</br> our industry-leading</br> calculator </p>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="our_process_lower_div_icon_div text-center">
+              <img class="" src="{{ asset('/front_asset/front_pages_asset/img/homepage_our_process_passenger_with_bag.png') }}" alt="">
+            </div>
+            <div class="our_process_lower_div_icon_text_div text-center">
+              <p>Add passenger details </br> then submit your claim </p>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="our_process_lower_div_icon_div text-center">
+              <img class="" src="{{ asset('/front_asset/front_pages_asset/img/homepage_our_process_collaboration.png') }}" alt="">
+            </div>
+            <div class="our_process_lower_div_icon_text_div text-center">
+              <p>We negotiate with the </br> airline for you and take it </br> to court in necessary </p>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="our_process_lower_div_icon_div text-center">
+              <img class="" src="{{ asset('/front_asset/front_pages_asset/img/homepage_our_process_cash_in_hand.png') }}" alt="">
+            </div>
+            <div class="our_process_lower_div_icon_text_div text-center">
+              <p>Your claim is settled and </br> your compensation </br> paid to you </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- Our Process div ends -->
+
+  <!-- Free Process div Starts -->
+  <div class="about_us_middle_content">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8 col-sm-12 col-xs-12 col-12">
+          <div class="about_us_middle_content_paragraph">
+            <p>Our Services are 100% no win no fee, </br>meaning there's no financial risk to you, </br> even if your claim is unsuccessfull.</p>
+          </div>
+        </div>
+      </div>
+      <div class="about_us_middle_content_button text-center">
+        <button type="button" name="button">CLAIM MY MONEY</button>
+      </div>
+    </div>
+  </div>
+  <!-- Free Process div ends -->
+
+
+
+
+@endsection
+
+@section('footer-script')
+  <script src="{{asset('front_asset/front_pages_asset/js/slick.js')}}"></script>
+  <script type="text/javascript">
+    $(".slider-area").slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1
+      });
+  </script>
 @endsection
