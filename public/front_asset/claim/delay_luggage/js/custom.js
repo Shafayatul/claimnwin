85f655c561dd737fb5faf5b48c1191484bf3d01a
Shafayatul <aabir68@yahoo.com>
2019-05-20 23:31:21 +0600

    extra space removed in public/front_asset/claim/delay_luggage/js/custom.js

--- public/front_asset/claim/delay_luggage/js/custom.js
+++ public/front_asset/claim/delay_luggage/js/custom.js
@@ -126,12 +126,10 @@ $(document).ready(function() {
 
     function ajax_calculation(){
 
-
       var is_already_written_airline  = $("input[name='is_already_written_airline']:checked").val();
       var written_airline_date        = $("input[name='written_airline_date']").val();
       var departure_date              = $(".departure_airport_date").val();
       var received_luggage_date       = $("input[name='received_luggage_date']").val();
-
 
       $.ajax({
         headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
@@ -149,7 +147,6 @@ $(document).ready(function() {
             $(".result_from_ajax_calculation").html('<div class="form_h3 text-center"><h3>SORRY!!!</h3></div><div class="form_show_message_paragraph"><p>Unfortunately, this flight is not eligible for compensation.Your claim details do not meet the criteria of Israeli or EU law to be compensated. Eligibility is calculated according the length of delay, air carriers and routes you have travelled on.</p></div>');
              $("#continue_5").hide();
           }else{
-
             $.ajax({
               headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
               type: 'POST',
@@ -169,8 +166,6 @@ $(document).ready(function() {
                 $(".result_from_ajax_calculation").html('<div class="form_h3 text-center"><h3>CONGRATULATIONS!!!</h3></div><div class="form_show_message_paragraph"><p>You are eligible for compensation. Your claim amount will be up to '+finalAmount+'</p></div>');
               }
             });
-
-
           }
         },
         error: function(e) {
