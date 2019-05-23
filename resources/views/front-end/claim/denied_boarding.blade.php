87c66a9a7f61731cb3f9dba8665804b3e7f9358c
mdsharafat <mdsharafat047@gmail.com>
2019-05-23 12:03:43 +0600

    Loading Js added to Denied Boarding

--- resources/views/front-end/claim/denied_boarding.blade.php
+++ resources/views/front-end/claim/denied_boarding.blade.php
@@ -755,7 +755,7 @@
                               <div class="total_button_div">
                                 <div class="two_button_child_div_right">
                                   <div class="continue_button_div">
-                                    <button type="button" class="continue_button pull-right" id="continue_3" name="button">Continue <i class="fas fa-arrow-right"></i></button>
+                                    <button type="button" onclick="horizontalCustomStyle();" class="continue_button pull-right" id="continue_3" name="button">Continue <i class="fas fa-arrow-right"></i></button>
                                   </div>
                                 </div>
                                 <div class="two_button_child_div_left">
@@ -1221,6 +1221,22 @@
   </script> --}}
 
   <script>
+  function loadingOut(loading) {
+  				setTimeout(() => loading.out(), 2000);
+  			}
+
+  function horizontalCustomStyle() {
+
+        var loading = new Loading({
+          title: ' Please wait',
+          direction: 'hor',
+          discription: 'Calculating...',
+            defaultApply: 	true,
+        });
+
+        loadingOut(loading);
+      }
+
     $(document).ready(function() {
             $('#signArea1').signaturePad({drawOnly:true, drawBezierCurves:true, lineTop:90});
             });
