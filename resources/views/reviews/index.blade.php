0662af32b349918d343ecc2d935d6aedf20258d3
SajalKundu <sajalkundu098@gmail.com>
2019-06-09 13:48:17 +0600

    Index File Modified in Reviews

--- resources/views/reviews/index.blade.php
+++ resources/views/reviews/index.blade.php
@@ -1,61 +1,74 @@
 @extends('layouts.admin_layout')
 
 @section('main_content')
-  <div class="card">
-      <div class="card-header">Reviews</div>
-      <div class="card-body">
-          <a href="{{ url('/reviews/create') }}" class="btn btn-success btn-sm" title="Add New Review">
-              <i class="fa fa-plus" aria-hidden="true"></i> Add New
-          </a>
+@include('layouts.includes.partial.alert')
+<div class="forms">
+    <div class="form-grids row widget-shadow" data-example-id="basic-forms">
+        <div class="form-title">
+            <h4>Reviews</h4>
+        </div>
+        <div class="form-body">
+            <div class="card">
+                <div class="card-body">
+                    {{-- <a href="{{ url('/reviews/create') }}" class="btn btn-success btn-sm" title="Add New Review">
+                        <i class="fa fa-plus" aria-hidden="true"></i> Add New
+                    </a>
 
-          {!! Form::open(['method' => 'GET', 'url' => '/reviews', 'class' => 'form-inline my-2 my-lg-0 float-right', 'role' => 'search'])  !!}
-          <div class="input-group">
-              <input type="text" class="form-control" name="search" placeholder="Search..." value="{{ request('search') }}">
-              <span class="input-group-append">
-                  <button class="btn btn-secondary" type="submit">
-                      <i class="fa fa-search"></i>
-                  </button>
-              </span>
-          </div>
-          {!! Form::close() !!}
+                    {!! Form::open(['method' => 'GET', 'url' => '/reviews', 'class' => 'form-inline my-2 my-lg-0 float-right', 'role' => 'search'])  !!}
+                    <div class="input-group">
+                        <input type="text" class="form-control" name="search" placeholder="Search..." value="{{ request('search') }}">
+                        <span class="input-group-append">
+                            <button class="btn btn-secondary" type="submit">
+                                <i class="fa fa-search"></i>
+                            </button>
+                        </span>
+                    </div>
+                    {!! Form::close() !!}
 
-          <br/>
-          <br/>
-          <div class="table-responsive">
-              <table class="table table-borderless">
-                  <thead>
-                      <tr>
-                          <th>#</th><th>Star</th><th>Title</th><th>Description</th><th>Actions</th>
-                      </tr>
-                  </thead>
-                  <tbody>
-                  @foreach($reviews as $item)
-                      <tr>
-                          <td>{{ $loop->iteration }}</td>
-                          <td>{{ $item->star }}</td><td>{{ $item->title }}</td><td>{{ $item->description }}</td>
-                          <td>
-                              <a href="{{ url('/reviews/' . $item->id) }}" title="View Review"><button class="btn btn-info btn-sm"><i class="fa fa-eye" aria-hidden="true"></i> View</button></a>
-                              <a href="{{ url('/reviews/' . $item->id . '/edit') }}" title="Edit Review"><button class="btn btn-primary btn-sm"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</button></a>
-                              {!! Form::open([
-                                  'method'=>'DELETE',
-                                  'url' => ['/reviews', $item->id],
-                                  'style' => 'display:inline'
-                              ]) !!}
-                                  {!! Form::button('<i class="fa fa-trash-o" aria-hidden="true"></i> Delete', array(
-                                          'type' => 'submit',
-                                          'class' => 'btn btn-danger btn-sm',
-                                          'title' => 'Delete Review',
-                                          'onclick'=>'return confirm("Confirm delete?")'
-                                  )) !!}
-                              {!! Form::close() !!}
-                          </td>
-                      </tr>
-                  @endforeach
-                  </tbody>
-              </table>
-              <div class="pagination-wrapper"> {!! $reviews->appends(['search' => Request::get('search')])->render() !!} </div>
-          </div>
+                    <br/>
+                    <br/> --}}
+                    <div class="table-responsive">
+                        <table class="table table-borderless">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Star</th>
+                                    <th>Title</th>
+                                    <th>Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($reviews as $item)
+                                <tr>
+                                    <td>{{ $loop->iteration }}</td>
+                                    <td>{{ $item->star }}</td>
+                                    <td>{{ $item->title }}</td>
+                                    <td>
+                                        <a href="{{ url('/reviews/' . $item->id) }}" title="View Review"><button class="btn btn-info btn-sm"><i class="fa fa-eye" aria-hidden="true"></i> View</button></a>
+                                        <a href="{{ url('/reviews/' . $item->id . '/edit') }}" title="Edit Review"><button class="btn btn-primary btn-sm"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</button></a>
+                                        {!! Form::open([
+                                            'method'=>'DELETE',
+                                            'url' => ['/reviews', $item->id],
+                                            'style' => 'display:inline'
+                                        ]) !!}
+                                            {!! Form::button('<i class="fa fa-trash" aria-hidden="true"></i> Delete', array(
+                                                    'type' => 'submit',
+                                                    'class' => 'btn btn-danger btn-sm',
+                                                    'title' => 'Delete Review',
+                                                    'onclick'=>'return confirm("Confirm delete?")'
+                                            )) !!}
+                                        {!! Form::close() !!}
+                                    </td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                        <div class="pagination-wrapper"> {!! $reviews->appends(['search' => Request::get('search')])->render() !!} </div>
+                    </div>
 
-      </div>
-  </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
