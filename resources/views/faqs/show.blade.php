d771af47707eb3860d4419d1c01007f37ee7c7a4
SajalKundu <sajalkundu098@gmail.com>
2019-06-09 13:00:10 +0600

    Faq Show File Modified

--- resources/views/faqs/show.blade.php
+++ resources/views/faqs/show.blade.php
@@ -1,38 +1,54 @@
 @extends('layouts.admin_layout')
-
 @section('main_content')
-  <div class="card">
-      <div class="card-header">Faq {{ $faq->id }}</div>
-      <div class="card-body">
+@include('layouts.includes.partial.alert')
+<div class="forms">
+    <div class="form-grids row widget-shadow" data-example-id="basic-forms">
+        <div class="form-title">
+            <h4>Faq {{ $faq->id }}</h4>
+        </div>
+        <div class="form-body">
+            <div class="card">
+                <div class="card-body">
 
-          <a href="{{ url('/faqs') }}" title="Back"><button class="btn btn-warning btn-sm"><i class="fa fa-arrow-left" aria-hidden="true"></i> Back</button></a>
-          <a href="{{ url('/faqs/' . $faq->id . '/edit') }}" title="Edit Faq"><button class="btn btn-primary btn-sm"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</button></a>
-          {!! Form::open([
-              'method'=>'DELETE',
-              'url' => ['faqs', $faq->id],
-              'style' => 'display:inline'
-          ]) !!}
-              {!! Form::button('<i class="fa fa-trash-o" aria-hidden="true"></i> Delete', array(
-                      'type' => 'submit',
-                      'class' => 'btn btn-danger btn-sm',
-                      'title' => 'Delete Faq',
-                      'onclick'=>'return confirm("Confirm delete?")'
-              ))!!}
-          {!! Form::close() !!}
-          <br/>
-          <br/>
+                    <a href="{{ url('/faqs') }}" title="Back"><button class="btn btn-warning btn-sm"><i class="fa fa-arrow-left" aria-hidden="true"></i> Back</button></a>
+                    <a href="{{ url('/faqs/' . $faq->id . '/edit') }}" title="Edit Faq"><button class="btn btn-primary btn-sm"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</button></a>
+                    {!! Form::open([
+                        'method'=>'DELETE',
+                        'url' => ['faqs', $faq->id],
+                        'style' => 'display:inline'
+                    ]) !!}
+                        {!! Form::button('<i class="fa fa-trash-o" aria-hidden="true"></i> Delete', array(
+                                'type' => 'submit',
+                                'class' => 'btn btn-danger btn-sm',
+                                'title' => 'Delete Faq',
+                                'onclick'=>'return confirm("Confirm delete?")'
+                        ))!!}
+                    {!! Form::close() !!}
+                    <br/>
+                    <br/>
 
-          <div class="table-responsive">
-              <table class="table table-borderless">
-                  <tbody>
-                      <tr>
-                          <th>ID</th><td>{{ $faq->id }}</td>
-                      </tr>
-                      <tr><th> Title </th><td> {{ $faq->title }} </td></tr><tr><th> Content </th><td> {{ $faq->content }} </td></tr>
-                  </tbody>
-              </table>
-          </div>
+                    <div class="table-responsive">
+                        <table class="table table-borderless">
+                            <tbody>
+                                <tr>
+                                    <th>ID</th>
+                                    <td>{{ $faq->id }}</td>
+                                </tr>
+                                <tr>
+                                    <th> Title </th>
+                                    <td> {{ $faq->title }} </td>
+                                </tr>
+                                <tr>
+                                    <th> Content </th>
+                                    <td> {{ $faq->content }} </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
 
-      </div>
-  </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
