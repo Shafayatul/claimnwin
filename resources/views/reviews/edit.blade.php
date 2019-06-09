98925cd7cbf969f1d9a18578c319acce250b1ae6
SajalKundu <sajalkundu098@gmail.com>
2019-06-09 13:45:48 +0600

    Edit Module Complete in Reviews

--- resources/views/reviews/edit.blade.php
+++ resources/views/reviews/edit.blade.php
@@ -1,32 +1,36 @@
 @extends('layouts.admin_layout')
-
 @section('main_content')
-  <div class="card">
-      <div class="card-header">Edit Review #{{ $review->id }}</div>
-      <div class="card-body">
-          <a href="{{ url('/reviews') }}" title="Back"><button class="btn btn-warning btn-sm"><i class="fa fa-arrow-left" aria-hidden="true"></i> Back</button></a>
-          <br />
-          <br />
+@include('layouts.includes.partial.alert')
+<div class="forms">
+    <div class="form-grids row widget-shadow" data-example-id="basic-forms">
+        <div class="form-title">
+            <h4>Edit Review #{{ $review->id }}</h4>
+        </div>
+        <div class="form-body">
+            <div class="card">
+                <div class="card-body">
 
-          @if ($errors->any())
-              <ul class="alert alert-danger">
-                  @foreach ($errors->all() as $error)
-                      <li>{{ $error }}</li>
-                  @endforeach
-              </ul>
-          @endif
+                    @if ($errors->any())
+                        <ul class="alert alert-danger">
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    @endif
 
-          {!! Form::model($review, [
-              'method' => 'PATCH',
-              'url' => ['/reviews', $review->id],
-              'class' => 'form-horizontal',
-              'files' => true
-          ]) !!}
+                    {!! Form::model($review, [
+                        'method' => 'PATCH',
+                        'url' => ['/reviews', $review->id],
+                        'files' => true
+                    ]) !!}
 
-          @include ('reviews.form', ['formMode' => 'edit'])
+                    @include ('reviews.form', ['formMode' => 'edit'])
 
-          {!! Form::close() !!}
+                    {!! Form::close() !!}
 
-      </div>
-  </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
