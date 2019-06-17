6756b963457bcb6074366c76f6ec4f328d35d7e8
mdsharafat <mdsharafat047@gmail.com>
2019-06-17 12:05:32 +0600

    flag added for language

--- resources/views/layouts/includes/front/header.blade.php
+++ resources/views/layouts/includes/front/header.blade.php
@@ -29,74 +29,74 @@
 
                 <li class="dropdown country">
                   <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-                    <img src="{{asset('front_asset/')}}/img/flag.png" alt="Flag"> 
+                    <img src="{{asset('front_asset/')}}/img/flag.png" alt="Flag">
                     <span class="caret"></span>
                   </a>
                   <ul class="dropdown-menu">
-                    <li><a href="{{url('change-language/af')}}">Afrikaans</a></li>
-                    <li><a href="{{url('change-language/ga')}}">Irish</a></li>
-                    <li><a href="{{url('change-language/sq')}}">Albanian</a></li> 
-                    <li><a href="{{url('change-language/it')}}" >Italian</a></li>
-                    <li><a href="{{url('change-language/ar')}}">Arabic</a></li> 
-                    <li><a href="{{url('change-language/ja')}}">Japanese</a></li>
-                    <li><a href="{{url('change-language/az')}}">Azerbaijani</a></li>
-                    <li><a href="{{url('change-language/kn')}}">Kannada</a></li>
-                    <li><a href="{{url('change-language/eu')}}">Basque</a></li> 
-                    <li><a href="{{url('change-language/ko')}}">Korean</a></li>
-                    <li><a href="{{url('change-language/bn')}}">Bengali</a></li>
-                    <li><a href="{{url('change-language/la')}}">Latin</a></li>
-                    <li><a href="{{url('change-language/be')}}">Belarusian</a></li> 
-                    <li><a href="{{url('change-language/lv')}}">Latvian</a></li>
-                    <li><a href="{{url('change-language/bg')}}">Bulgarian</a></li>
-                    <li><a href="{{url('change-language/lt')}}">Lithuanian</a></li>
-                    <li><a href="{{url('change-language/ca')}}">Catalan</a></li>
-                    <li><a href="{{url('change-language/mk')}}">Macedonian</a></li>
-                    <li><a href="{{url('change-language/zh-CN')}}">Chinese Simplified</a></li>
-                    <li><a href="{{url('change-language/ms')}}">Malay</a></li>
-                    <li><a href="{{url('change-language/zh-TW')}}">Chinese Traditional</a></li>
-                    <li><a href="{{url('change-language/mt')}}">Maltese</a></li>
-                    <li><a href="{{url('change-language/hr')}}">Croatian</a></li> 
-                    <li><a href="{{url('change-language/no')}}">Norwegian</a></li>
-                    <li><a href="{{url('change-language/cs')}}">Czech</a></li>
-                    <li><a href="{{url('change-language/fa')}}">Persian</a></li>
-                    <li><a href="{{url('change-language/da')}}">Danish</a></li> 
-                    <li><a href="{{url('change-language/pl')}}">Polish</a></li>
-                    <li><a href="{{url('change-language/nl')}}">Dutch</a></li>
-                    <li><a href="{{url('change-language/pt')}}">Portuguese</a></li>
-                    <li><a href="{{url('change-language/en')}}">English</a></li>
-                    <li><a href="{{url('change-language/ro')}}">Romanian</a></li>
-                    <li><a href="{{url('change-language/eo')}}">Esperanto</a></li>
-                    <li><a href="{{url('change-language/ru')}}">Russian</a></li>
-                    <li><a href="{{url('change-language/et')}}">Estonian</a></li> 
-                    <li><a href="{{url('change-language/sr')}}">Serbian</a></li>
-                    <li><a href="{{url('change-language/tl')}}">Filipino</a></li> 
-                    <li><a href="{{url('change-language/sk')}}">Slovak</a></li>
-                    <li><a href="{{url('change-language/fi')}}">Finnish</a></li>
-                    <li><a href="{{url('change-language/sl')}}">Slovenian</a></li>
-                    <li><a href="{{url('change-language/fr')}}">French</a></li> 
-                    <li><a href="{{url('change-language/es')}}">Spanish</a></li>
-                    <li><a href="{{url('change-language/gl')}}">Galician</a></li> 
-                    <li><a href="{{url('change-language/sw')}}">Swahili</a></li>
-                    <li><a href="{{url('change-language/ka')}}">Georgian</a></li> 
-                    <li><a href="{{url('change-language/sv')}}">Swedish</a></li>
-                    <li><a href="{{url('change-language/de')}}">German</a></li> 
-                    <li><a href="{{url('change-language/ta')}}">Tamil</a></li>
-                    <li><a href="{{url('change-language/el')}}">Greek</a></li>
-                    <li><a href="{{url('change-language/te')}}">Telugu</a></li>
-                    <li><a href="{{url('change-language/gu')}}">Gujarati</a></li> 
-                    <li><a href="{{url('change-language/th')}}">Thai</a></li>
-                    <li><a href="{{url('change-language/ht')}}">Haitian Creole</a></li>
-                    <li><a href="{{url('change-language/tr')}}">Turkish</a></li>
-                    <li><a href="{{url('change-language/iw')}}">Hebrew</a></li> 
-                    <li><a href="{{url('change-language/uk')}}">Ukrainian</a></li>
-                    <li><a href="{{url('change-language/hi')}}">Hindi</a></li>
-                    <li><a href="{{url('change-language/ur')}}">Urdu</a></li>
-                    <li><a href="{{url('change-language/hu')}}">Hungarian</a></li>
-                    <li><a href="{{url('change-language/vi')}}">Vietnamese</a></li>
-                    <li><a href="{{url('change-language/is')}}">Icelandic</a></li>
-                    <li><a href="{{url('change-language/cy')}}">Welsh</a></li>
-                    <li><a href="{{url('change-language/id')}}">Indonesian</a></li> 
-                    <li><a href="{{url('change-language/yi')}}">Yiddish</a></li>
+                    <li><a href="{{url('change-language/af')}}"> <img src="{{ asset('front_asset/img/country-flags/Flag_of_South_Africa.png')}}" alt=""></a></li>
+                    <li><a href="{{url('change-language/ga')}}"><img src="{{ asset('front_asset/img/country-flags/Flag_of_Ireland.png')}}" alt=""> </a></li>
+                    <li><a href="{{url('change-language/sq')}}"><img src="{{ asset('front_asset/img/country-flags/Flag_of_Albania.png')}}" alt=""> </a></li>
+                    <li><a href="{{url('change-language/it')}}" ><img src="{{ asset('front_asset/img/country-flags/Flag_of_Italy.png')}}" alt=""> </a></li>
+                    <li><a href="{{url('change-language/ar')}}"><img src="{{ asset('front_asset/img/country-flags/Flag_of_Saudi_Arabia.png')}}" alt=""> </a></li>
+                    <li><a href="{{url('change-language/ja')}}"><img src="{{ asset('front_asset/img/country-flags/Flag_of_Japan.png')}}" alt=""> </a></li>
+                    <li><a href="{{url('change-language/az')}}"><img src="{{ asset('front_asset/img/country-flags/Flag_of_Azerbaijan.png')}}" alt=""> </a></li>
+                    <li><a href="{{url('change-language/kn')}}"><img src="{{ asset('front_asset/img/country-flags/Flag_of_Canada.png')}}" alt=""> </a></li>
+                    <li><a href="{{url('change-language/eu')}}"> <img src="{{ asset('front_asset/img/country-flags/Flag_of_Spain.png')}}" alt=""> </a></li>
+                    <li><a href="{{url('change-language/ko')}}"> <img src="{{ asset('front_asset/img/country-flags/Flag_of_South_Korea.png')}}" alt=""> </a></li>
+                    <li><a href="{{url('change-language/bn')}}"> <img src="{{ asset('front_asset/img/country-flags/Flag_of_Bangladesh.png')}}" alt=""> </a></li>
+                    <li><a href="{{url('change-language/la')}}"> <img src="{{ asset('front_asset/img/country-flags/Flag_of_Italy.png')}}" alt=""> </a></li>
+                    <li><a href="{{url('change-language/be')}}"> <img src="{{ asset('front_asset/img/country-flags/Flag_of_Belarus.png')}}" alt=""> </a></li>
+                    <li><a href="{{url('change-language/lv')}}"> <img src="{{ asset('front_asset/img/country-flags/Flag_of_Latvia.png')}}" alt=""> </a></li>
+                    <li><a href="{{url('change-language/bg')}}"> <img src="{{ asset('front_asset/img/country-flags/Flag_of_Bulgaria.png')}}" alt=""> </a></li>
+                    <li><a href="{{url('change-language/lt')}}"> <img src="{{ asset('front_asset/img/country-flags/Flag_of_Lithuania.png')}}" alt=""> </a></li>
+                    {{-- <li><a href="{{url('change-language/ca')}}">Catalan</a></li> --}}
+                    <li><a href="{{url('change-language/mk')}}"> <img src="Flag_of_Macedonia.png" alt=""> </a></li>
+                    <li><a href="{{url('change-language/zh-CN')}}"> <img src="{{ asset('front_asset/img/country-flags/Flag_of_the_Peoples_Republic_of_China.png')}}" alt=""> </a></li>
+                    <li><a href="{{url('change-language/ms')}}"> <img src="{{ asset('front_asset/img/country-flags/Flag_of_Malaysia.png')}}" alt=""> </a></li>
+                    <li><a href="{{url('change-language/zh-TW')}}"> <img src="{{ asset('front_asset/img/country-flags/Flag_of_the_Peoples_Republic_of_China.png')}}" alt=""> </a></li>
+                    <li><a href="{{url('change-language/mt')}}"> <img src="{{ asset('front_asset/img/country-flags/Flag_of_Malta.png')}}" alt=""> </a></li>
+                    <li><a href="{{url('change-language/hr')}}"> <img src="{{ asset('front_asset/img/country-flags/Flag_of_Croatia.png')}}" alt=""> </a></li>
+                    <li><a href="{{url('change-language/no')}}"> <img src="{{ asset('front_asset/img/country-flags/Flag_of_Norway.png')}}" alt=""> </a></li>
+                    <li><a href="{{url('change-language/cs')}}"> <img src="{{ asset('front_asset/img/country-flags/Flag_of_the_Czech_Republic.png')}}" alt=""> </a></li>
+                    <li><a href="{{url('change-language/fa')}}"> <img src="{{ asset('front_asset/img/country-flags/Flag_of_Iran.png')}}" alt=""> </a></li>
+                    <li><a href="{{url('change-language/da')}}"> <img src="{{ asset('front_asset/img/country-flags/Flag_of_Denmark.png')}}" alt=""> </a></li>
+                    <li><a href="{{url('change-language/pl')}}"> <img src="{{ asset('front_asset/img/country-flags/Flag_of_Poland.png')}}" alt=""> </a></li>
+                    <li><a href="{{url('change-language/nl')}}"> <img src="{{ asset('front_asset/img/country-flags/Flag_of_the_Netherlands.png')}}" alt=""> </a></li>
+                    <li><a href="{{url('change-language/pt')}}"> <img src="{{ asset('front_asset/img/country-flags/Flag_of_Portugal.png')}}" alt=""> </a></li>
+                    <li><a href="{{url('change-language/en')}}"> <img src="{{asset('front_asset/')}}/img/phone.png" alt=""> </a></li>
+                    <li><a href="{{url('change-language/ro')}}"> <img src="{{ asset('front_asset/img/country-flags/Flag_of_Romania.png')}}" alt=""> </a></li>
+                    {{-- <li><a href="{{url('change-language/eo')}}">Esperanto</a></li> --}}
+                    <li><a href="{{url('change-language/ru')}}"> <img src="{{ asset('front_asset/img/country-flags/Flag_of_Russia.png')}}" alt=""> </a></li>
+                    <li><a href="{{url('change-language/et')}}"> <img src="{{ asset('front_asset/img/country-flags/Flag_of_Estonia.png')}}" alt=""> </a></li>
+                    <li><a href="{{url('change-language/sr')}}"> <img src="{{ asset('front_asset/img/country-flags/Flag_of_Serbia.png')}}" alt=""> </a></li>
+                    <li><a href="{{url('change-language/tl')}}"> <img src="{{ asset('front_asset/img/country-flags/Flag_of_the_Philippines.png')}}" alt=""> </a></li>
+                    <li><a href="{{url('change-language/sk')}}"> <img src="{{ asset('front_asset/img/country-flags/Flag_of_Slovakia.png')}}" alt=""> </a></li>
+                    <li><a href="{{url('change-language/fi')}}"> <img src="{{ asset('front_asset/img/country-flags/Flag_of_Finland.png')}}" alt=""> </a></li>
+                    <li><a href="{{url('change-language/sl')}}"> <img src="{{ asset('front_asset/img/country-flags/Flag_of_Slovenia.png')}}" alt=""> </a></li>
+                    <li><a href="{{url('change-language/fr')}}"> <img src="{{ asset('front_asset/img/country-flags/Flag_of_France.png')}}" alt=""> </a></li>
+                    <li><a href="{{url('change-language/es')}}"> <img src="{{ asset('front_asset/img/country-flags/Flag_of_Spain.png')}}" alt=""> </a></li>
+                    {{-- <li><a href="{{url('change-language/gl')}}">Galician</a></li> --}}
+                    <li><a href="{{url('change-language/sw')}}"> <img src="{{ asset('front_asset/img/country-flags/Flag_of_Kenya.png')}}" alt=""> </a></li>
+                    <li><a href="{{url('change-language/ka')}}"> <img src="{{ asset('front_asset/img/country-flags/Flag_of_Georgia.png')}}" alt=""> </a></li>
+                    <li><a href="{{url('change-language/sv')}}"> <img src="{{ asset('front_asset/img/country-flags/Flag_of_Sweden.png')}}" alt=""> </a></li>
+                    <li><a href="{{url('change-language/de')}}"> <img src="{{ asset('front_asset/img/country-flags/Flag_of_Germany.png')}}" alt=""> </a></li>
+                    {{-- <li><a href="{{url('change-language/ta')}}">Tamil</a></li> --}}
+                    <li><a href="{{url('change-language/el')}}"> <img src="{{ asset('front_asset/img/country-flags/Flag_of_Greece.png')}}" alt=""> </a></li>
+                    {{-- <li><a href="{{url('change-language/te')}}">Telugu</a></li> --}}
+                    {{-- <li><a href="{{url('change-language/gu')}}">Gujarati</a></li> --}}
+                    <li><a href="{{url('change-language/th')}}"> <img src="{{ asset('front_asset/img/country-flags/Flag_of_Thailand.png')}}" alt=""> </a></li>
+                    <li><a href="{{url('change-language/ht')}}"> <img src="{{ asset('front_asset/img/country-flags/Flag_of_Haiti.png')}}" alt=""> </a></li>
+                    <li><a href="{{url('change-language/tr')}}"> <img src="{{ asset('front_asset/img/country-flags/Flag_of_Turkey.png')}}" alt=""> </a></li>
+                    <li><a href="{{url('change-language/iw')}}"> <img src="{{ asset('front_asset/img/country-flags/Flag_of_Israel.png')}}" alt=""> </a></li>
+                    <li><a href="{{url('change-language/uk')}}"> <img src="{{ asset('front_asset/img/country-flags/Flag_of_Ukraine.png')}}" alt=""> </a></li>
+                    <li><a href="{{url('change-language/hi')}}"> <img src="{{ asset('front_asset/img/country-flags/Flag_of_India.png')}}" alt=""> </a></li>
+                    <li><a href="{{url('change-language/ur')}}"> <img src="{{ asset('front_asset/img/country-flags/Flag_of_Pakistan.png')}}" alt=""> </a></li>
+                    <li><a href="{{url('change-language/hu')}}"> <img src="{{ asset('front_asset/img/country-flags/Flag_of_Hungary.png')}}" alt=""> </a></li>
+                    <li><a href="{{url('change-language/vi')}}"> <img src="{{ asset('front_asset/img/country-flags/Flag_of_Vietnam.png')}}" alt=""> </a></li>
+                    <li><a href="{{url('change-language/is')}}"> <img src="{{ asset('front_asset/img/country-flags/Flag_of_Iceland.png')}}" alt=""> </a></li>
+                    {{-- <li><a href="{{url('change-language/cy')}}">Welsh</a></li> --}}
+                    <li><a href="{{url('change-language/id')}}"> <img src="{{ asset('front_asset/img/country-flags/Flag_of_Indonesia.png')}}" alt=""> </a></li>
+                    <li><a href="{{url('change-language/yi')}}"> <img src="{{ asset('front_asset/img/country-flags/Flag_of_Israel.png')}}" alt=""> </a></li>
                   </ul>
                 </li>
                 @guest
