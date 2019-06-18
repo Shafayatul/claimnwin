44a1bb6538c2b341343bdb8045be459eea755a64
mdsharafat <mdsharafat047@gmail.com>
2019-06-18 16:50:07 +0600

    terms n condition page google translator added

--- app/Http/Controllers/FrontsController.php
+++ app/Http/Controllers/FrontsController.php
@@ -82,7 +82,54 @@ class FrontsController extends Controller
     }
     public function termsAndConditions()
     {
-        return view('front-pages.terms_n_condition');
+      $text[0] = "Terms & Conditions";
+      $text[1] = "User Agreement";
+      $text[2] = "We may collect personal identification information from Users, including, but not limited to, when Users visit our site, register their";
+      $text[3] = "information on the website by completing, or partially completing the webform giving us full or partial instruction/ details of their";
+      $text[4] = "proposed instruction.";
+      $text[5] = "Users may be asked for, as appropriate, name, email address, mailing address, phone numbers, flight details and relevant information relating to the";
+      $text[6] = "circumstances of the event which the claim/ proposed claim relates to. Additional supporting information/evidence is also likely to be requested.We";
+      $text[7] = "may also request bank details if paying compensation directly into your account.";
+      $text[8] = "We will collect personal identification information from Users only if they submit such information to us. Users can always refuse to supply personal";
+      $text[9] = "identification information, except that it may prevent them from engaging our services to assist them in obtaining any due compensation from the";
+      $text[10] = "Carrier/Airline.";
+      $text[11] = "We will retain your personal data until we have completed the compensation claim fully, and any monies due have been paid/received, and the file has";
+      $text[12] = "been deemed closed, and a suitable period of time, (within a 3 month period) has lapsed to allow us to remove your details from our records.";
+      $text[13] = "In this User Agreement:";
+      $text[14] = "We may collect non-personal identification information about Users whenever they interact with E.Asthampton Limited trading as Claim Win Team,";
+      $text[15] = "whether via our site, emails or any other form of communication. Non-personal identification information may include the browser name, the type of";
+      $text[16] = "computer and technical information about Users means of connection to our Site, emails or other";
+      $text[17] = "electronic methods of communication, such as the operating system and the Internet service providers utilised and other similar";
+      $text[18] = "information, such as your IP address.";
+      $text[19] = "Using this site";
+      $text[20] = "E.Asthampton Limited, trading as Claim Win Team collects and uses personal information for the following perposes:";
+      $text[21] = "To improve customer experience by offering the User, the opportunity of engaging our services to assist in them in securing any";
+      $text[22] = "compensation due to them as a result of a delayed or cancelled flight, subject to a qualifying criteria, in line with EU 261/04, The";
+      $text[23] = "Montreal Convention, Regulation On Air Passenger Rights and US Code of Federal Regulations.";
+      $text[24] = "To process the claim. We will use the information the User has provided to process the claim you have engaged our services to";
+      $text[25] = "gain any due compensation. This will include us undertaking various eligibility checks, and contacting the airline/carrier. We may";
+      $text[26] = "need to contact you for additional information relating to the circumstances, documentation or your overall experience.";
+      $text[27] = "To make contact with you the User. Additional information may be required to assist us with your claim. We may need to advise ";
+      $text[28] = "you of information we receive from your Carrier in the process of undertaking our work. ";
+      $text[29] = "We may need to contact you the User for your bank details, should the Airline in question wish to pay you directly into your";
+      $text[30] = "account. We may also need to contact you with our invoice, or payment reminders, if payment has not been made directly to ";
+      $text[31] = "ourselves by the Airline in the first instance.";
+      $text[32] = "Fees and Services";
+      $text[33] = "On review of your claim by one of our experienced Claims Handlers, it may be considered that to maximise your chances of receiving";
+      $text[34] = "any due compensation, we need to engage one of our Panel Solicitors. This decision may be made at any point during the claim process,";
+      $text[35] = "and will then involve E.Asthampton Limited, trading as Claim Win Team, passing your data to the chosen Panel Solicitor which is";
+      $text[36] = "felt to be best placed to handle your claim.";
+      $text[37] = "We will contact you, using the data provided, requesting you to sign the Panel Solicitors instructions.";
+      $text[38] = "By signing this instruction, you are giving us permission to release your data to the Panel Solicitor in question.";
+      $text[39] = "We do not sell, or trade your data with any other organisation.";
+
+      if (Session::has('locale')) {
+        $responseDecoded = $this->get_translation($text);
+        return view('front-pages.terms_n_condition',compact('responseDecoded', 'text'));
+      }else {
+        $responseDecoded = null;
+        return view('front-pages.terms_n_condition',compact('responseDecoded', 'text'));
+      }
     }
     public function privacyPolicy()
     {
