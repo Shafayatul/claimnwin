a1289af94f915577674768d8d3940cfdc76f64e8
SajalKundu <sajalkundu098@gmail.com>
2019-06-18 17:00:44 +0600

    Privacy Policy Translate Complete

--- resources/views/front-pages/privacy_policy.blade.php
+++ resources/views/front-pages/privacy_policy.blade.php
@@ -5,7 +5,13 @@
 
 @section('page-title')
   <div class="page_title">
-    <h1 class="text-center">Privacy Policy</h1>
+    <h1 class="text-center">
+        @if ($responseDecoded)
+        {{ $responseDecoded['data']['translations'][0]['translatedText']}}
+        @else
+            {{ $text[0]}}
+        @endif
+    </h1>
   </div>
 @endsection
 
@@ -15,37 +21,99 @@
         <div class="row">
             <div class="col-md-12">
                 <div class="title_row">
-                    <h1>Personal Identification Information</h1>
+                    <h1>
+                        @if ($responseDecoded)
+                        {{ $responseDecoded['data']['translations'][1]['translatedText']}}
+                        @else
+                            {{ $text[1]}}
+                        @endif
+                    </h1>
                 </div>
                 <div class="first_row_p">
                     <p>
-                        We may collect personal identification information from Users, including, but not limited to, when Users visit our site, register their <br>
-                        information on the website by completing, or partially completing the webform giving us full or partial instruction/ details of their <br>
-                        proposed instruction.
+                        @if ($responseDecoded)
+                        {{ $responseDecoded['data']['translations'][2]['translatedText']}}
+                        @else
+                            {{ $text[2]}}
+                        @endif
+                        <br>
+                        @if ($responseDecoded)
+                        {{ $responseDecoded['data']['translations'][3]['translatedText']}}
+                        @else
+                            {{ $text[3]}}
+                        @endif
+                        <br>
+                        @if ($responseDecoded)
+                        {{ $responseDecoded['data']['translations'][4]['translatedText']}}
+                        @else
+                            {{ $text[4]}}
+                        @endif
                     </p>
                 </div>
 
                 <div class="first_row_p">
                     <p>
-                        Users may be asked for, as appropriate, name, email address, mailing address, phone numbers, flight details and relevant information relating to the <br>
-                        circumstances of the event which the claim/ proposed claim relates to. Additional supporting information/evidence is also likely to be requested.We <br>
-                        may also request bank details if paying compensation directly into your account.
+                        @if ($responseDecoded)
+                        {{ $responseDecoded['data']['translations'][5]['translatedText']}}
+                        @else
+                            {{ $text[5]}}
+                        @endif
+                        <br>
+                        @if ($responseDecoded)
+                        {{ $responseDecoded['data']['translations'][6]['translatedText']}}
+                        @else
+                            {{ $text[6]}}
+                        @endif
+                        <br>
+                        @if ($responseDecoded)
+                        {{ $responseDecoded['data']['translations'][7]['translatedText']}}
+                        @else
+                            {{ $text[7]}}
+                        @endif
                     </p>
                 </div>
 
                 <div class="first_row_p">
                     <p>
-                        We will collect personal identification information from Users only if they submit such information to us. Users can always refuse to supply personal <br>
-                        identification information, except that it may prevent them from engaging our services to assist them in obtaining any due compensation from the <br>
-                        Carrier/Airline.
+                        @if ($responseDecoded)
+                        {{ $responseDecoded['data']['translations'][8]['translatedText']}}
+                        @else
+                            {{ $text[8]}}
+                        @endif
+                        <br>
+                        @if ($responseDecoded)
+                        {{ $responseDecoded['data']['translations'][9]['translatedText']}}
+                        @else
+                            {{ $text[9]}}
+                        @endif
+                        <br>
+                        @if ($responseDecoded)
+                        {{ $responseDecoded['data']['translations'][10]['translatedText']}}
+                        @else
+                            {{ $text[10]}}
+                        @endif
                     </p>
                 </div>
 
                 <div class="first_row_p no_bottom_padding">
                     <p>
-                        We will retain your personal data until we have completed the compensation claim fully, and any monies due have been paid/received,<br>
-                        and the file has been deemed closed, and a suitable period of time, (within a 3 month period) has lapsed to allow us to remove your <br>
-                        details from our records.
+                        @if ($responseDecoded)
+                        {{ $responseDecoded['data']['translations'][11]['translatedText']}}
+                        @else
+                            {{ $text[11]}}
+                        @endif
+                        <br>
+                        @if ($responseDecoded)
+                        {{ $responseDecoded['data']['translations'][12]['translatedText']}}
+                        @else
+                            {{ $text[12]}}
+                        @endif
+                        <br>
+                        @if ($responseDecoded)
+                        {{ $responseDecoded['data']['translations'][13]['translatedText']}}
+                        @else
+                            {{ $text[13]}}
+                        @endif
                     </p>
                 </div>
             </div>
@@ -56,15 +124,45 @@
         <div class="row">
             <div class="col-md-12">
                 <div class="title_row">
-                    <h1>Non Personal Identification Information</h1>
+                    <h1>
+                        @if ($responseDecoded)
+                        {{ $responseDecoded['data']['translations'][14]['translatedText']}}
+                        @else
+                            {{ $text[14]}}
+                        @endif
+                    </h1>
                 </div>
                 <div class="second_row_p">
                     <p>
-                        We may collect non-personal identification information about Users whenever they interact with E.Asthampton Limited trading as <br> Claim Win Team,
-                        whether via our site, emails or any other form of communication. Non-personal identification information may include <br> the browser name, the type of
-                        computer and technical information about Users means of connection to our Site, emails or other <br>
-                        electronic methods of communication, such as the operating system and the Internet service providers utilised and other similar <br>
-                        information, such as your IP address.
+                        @if ($responseDecoded)
+                        {{ $responseDecoded['data']['translations'][15]['translatedText']}}
+                        @else
+                            {{ $text[15]}}
+                        @endif
+                        <br>
+                        @if ($responseDecoded)
+                        {{ $responseDecoded['data']['translations'][16]['translatedText']}}
+                        @else
+                            {{ $text[16]}}
+                        @endif
+                        <br>
+                        @if ($responseDecoded)
+                        {{ $responseDecoded['data']['translations'][17]['translatedText']}}
+                        @else
+                            {{ $text[17]}}
+                        @endif
+                        <br>
+                        @if ($responseDecoded)
+                        {{ $responseDecoded['data']['translations'][18]['translatedText']}}
+                        @else
+                            {{ $text[18]}}
+                        @endif
+                        <br>
+                        @if ($responseDecoded)
+                        {{ $responseDecoded['data']['translations'][19]['translatedText']}}
+                        @else
+                            {{ $text[19]}}
+                        @endif
                     </p>
                 </div>
             </div>
@@ -75,31 +173,93 @@
         <div class="row">
             <div class="col-md-12">
                 <div class="title_row">
-                    <h1>How We Use Collected Information</h1>
+                    <h1>
+                        @if ($responseDecoded)
+                        {{ $responseDecoded['data']['translations'][20]['translatedText']}}
+                        @else
+                            {{ $text[20]}}
+                        @endif
+                    </h1>
                 </div>
                 <div class="third_row_p">
                     <p>
-                        E.Asthampton Limited, trading as Claim Win Team collects and uses personal information for the following perposes:
+                        @if ($responseDecoded)
+                        {{ $responseDecoded['data']['translations'][21]['translatedText']}}
+                        @else
+                            {{ $text[21]}}
+                        @endif
+                        :
                     </p>
                     <ul>
                         <li>
-                            To improve customer experience by offering the User, the opportunity of engaging our services to assist in them in securing any <br>
-                            compensation due to them as a result of a delayed or cancelled flight, subject to a qualifying criteria, in line with EU 261/04, The <br>
-                            Montreal Convention, Regulation On Air Passenger Rights and US Code of Federal Regulations.
+                            @if ($responseDecoded)
+                            {{ $responseDecoded['data']['translations'][22]['translatedText']}}
+                            @else
+                                {{ $text[22]}}
+                            @endif
+                            <br>
+                            @if ($responseDecoded)
+                            {{ $responseDecoded['data']['translations'][23]['translatedText']}}
+                            @else
+                                {{ $text[23]}}
+                            @endif
+                            <br>
+                            @if ($responseDecoded)
+                            {{ $responseDecoded['data']['translations'][24]['translatedText']}}
+                            @else
+                                {{ $text[24]}}
+                            @endif
                         </li>
                         <li>
-                            To process the claim. We will use the information the User has provided to process the claim you have engaged our services to <br>
-                            gain any due compensation. This will include us undertaking various eligibility checks, and contacting the airline/carrier. We may <br>
-                            need to contact you for additional information relating to the circumstances, documentation or your overall experience.
+                            @if ($responseDecoded)
+                            {{ $responseDecoded['data']['translations'][25]['translatedText']}}
+                            @else
+                                {{ $text[25]}}
+                            @endif
+                            <br>
+                            @if ($responseDecoded)
+                            {{ $responseDecoded['data']['translations'][26]['translatedText']}}
+                            @else
+                                {{ $text[26]}}
+                            @endif
+                            <br>
+                            @if ($responseDecoded)
+                            {{ $responseDecoded['data']['translations'][27]['translatedText']}}
+                            @else
+                                {{ $text[27]}}
+                            @endif
                         </li>
                         <li>
-                            To make contact with you the User. Additional information may be required to assist us with your claim. We may need to advise <br>
-                            you of information we receive from your Carrier in the process of undertaking our work.
+                            @if ($responseDecoded)
+                            {{ $responseDecoded['data']['translations'][28]['translatedText']}}
+                            @else
+                                {{ $text[28]}}
+                            @endif
+                            <br>
+                            @if ($responseDecoded)
+                            {{ $responseDecoded['data']['translations'][29]['translatedText']}}
+                            @else
+                                {{ $text[29]}}
+                            @endif
                         </li>
                         <li>
-                            We may need to contact you the User for your bank details, should the Airline in question wish to pay you directly into your <br>
-                            account. We may also need to contact you with our invoice, or payment reminders, if payment has not been made directly to <br>
-                            ourselves by the Airline in the first instance.
+                            @if ($responseDecoded)
+                            {{ $responseDecoded['data']['translations'][30]['translatedText']}}
+                            @else
+                                {{ $text[30]}}
+                            @endif
+                            <br>
+                            @if ($responseDecoded)
+                            {{ $responseDecoded['data']['translations'][31]['translatedText']}}
+                            @else
+                                {{ $text[31]}}
+                            @endif
+                            <br>
+                            @if ($responseDecoded)
+                            {{ $responseDecoded['data']['translations'][32]['translatedText']}}
+                            @else
+                                {{ $text[32]}}
+                            @endif
                         </li>
                     </ul>
                 </div>
@@ -111,29 +271,66 @@
         <div class="row">
             <div class="col-md-12">
                 <div class="title_row">
-                    <h1>Sharing Your Personal Information</h1>
+                    <h1>
+                        @if ($responseDecoded)
+                            {{ $responseDecoded['data']['translations'][33]['translatedText']}}
+                        @else
+                            {{ $text[33]}}
+                        @endif
+                    </h1>
                 </div>
                 <div class="fourth_row_p">
                     <p>
-                        On review of your claim by one of our experienced Claims Handlers, it may be considered that to maximise your chances of receiving <br>
-                        any due compensation, we need to engage one of our Panel Solicitors. This decision may be made at any point during the claim process, <br>
-                        and will then involve E.Asthampton Limited, trading as Claim Win Team, passing your data to the chosen Panel Solicitor which is <br>
-                        felt to be best placed to handle your claim.
+                        @if ($responseDecoded)
+                            {{ $responseDecoded['data']['translations'][34]['translatedText']}}
+                        @else
+                            {{ $text[34]}}
+                        @endif
+                        <br>
+                        @if ($responseDecoded)
+                            {{ $responseDecoded['data']['translations'][35]['translatedText']}}
+                        @else
+                            {{ $text[35]}}
+                        @endif
+                        <br>
+                        @if ($responseDecoded)
+                            {{ $responseDecoded['data']['translations'][36]['translatedText']}}
+                        @else
+                            {{ $text[36]}}
+                        @endif
+                        <br>
+                        @if ($responseDecoded)
+                            {{ $responseDecoded['data']['translations'][37]['translatedText']}}
+                        @else
+                            {{ $text[37]}}
+                        @endif
                     </p>
                 </div>
                 <div class="fourth_row_p">
                     <p>
-                        We will contact you, using the data provided, requesting you to sign the Panel Solicitors instructions.
+                        @if ($responseDecoded)
+                            {{ $responseDecoded['data']['translations'][38]['translatedText']}}
+                        @else
+                            {{ $text[38]}}
+                        @endif
                     </p>
                 </div>
                 <div class="fourth_row_p">
                     <p>
-                        By signing this instruction, you are giving us permission to release your data to the Panel Solicitor in question.
+                        @if ($responseDecoded)
+                            {{ $responseDecoded['data']['translations'][39]['translatedText']}}
+                        @else
+                            {{ $text[39]}}
+                        @endif
                     </p>
                 </div>
                 <div class="fourth_row_p no_bottom_padding">
                     <p>
-                        We do not sell, or trade your data with any other organisation.
+                        @if ($responseDecoded)
+                            {{ $responseDecoded['data']['translations'][40]['translatedText']}}
+                        @else
+                            {{ $text[40]}}
+                        @endif
                     </p>
                 </div>
             </div>
