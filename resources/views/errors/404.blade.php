804098d9e82005c1b159da788d18407c5f69d826
Shafayatul <aabir68@yahoo.com>
2019-06-26 19:52:39 +0600

    404 and 403 page edited

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,17 +1,17 @@
-<!--
-Author: W3layouts
-Author URL: http://w3layouts.com
-License: Creative Commons Attribution 3.0 Unported
-License URL: http://creativecommons.org/licenses/by/3.0/
--->
-<!DOCTYPE html>
-<html>
-<head>
-<title>404 Error</title>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta name="keywords" content="Trouble 404 Error Page template Responsive, Login form web template,Flat Pricing tables,Flat Drop downs  Sign up Web Templates, Flat Web Templates, Login sign up Responsive web template, SmartPhone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
-<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+@extends('layouts.front_layout')
+@section('header-script')
+<link  href="{{asset('front_asset/front_pages_asset/css/press_blog.css')}}" rel="stylesheet">
+@endsection
+
+@section('page-title')
+  <div class="page_title">
+    <h1 class="text-center">
+      Trouble 404
+    </h1>
+  </div>
+@endsection
+
+@section('content')
 <!-- Custom Theme files -->
 <link href="{{asset('error/')}}/css/style.css" rel="stylesheet" type="text/css" media="all" />
 <link href="{{asset('error/')}}/css/font-awesome.css" rel="stylesheet"> <!-- font-awesome icons -->
@@ -19,33 +19,31 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 <!-- web font -->
 <link href="//fonts.googleapis.com/css?family=Archivo+Black" rel="stylesheet">
 <link href='//fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800' rel='stylesheet' type='text/css'><!--web font-->
-<!-- //web font -->
-</head>
-<body>
-	<!-- main -->
-	<div class="banner-dott banner-bg">
-	<div class="agileits-main">
-		<div class="agileinfo-row">
-			<div class="w3top-nav">
-				<div class="w3top-nav-left">
-				<h1><a href="{{URL::to('/')}}">Trouble 404</a></h1>
+<div class="container">
+    <div class="first_row">
+        <div class="row">
+            <div class="col-md-12">
+				<div {{-- class="banner-dott banner-bg" --}}>
+					<div class="agileits-main">
+						<div class="agileinfo-row">
+							{{-- <div class="w3top-nav">
+								<div class="w3top-nav-left">
+								<h1><a href="{{URL::to('/')}}">Trouble 404</a></h1>
+								</div>
+								<div class="clear"></div>
+							</div> --}}
+
+							<div class="w3layouts-errortext">
+								<h2><span>4</span> <span><i class="fa fa-circle-o-notch" aria-hidden="true"></i></span> <span>4</span></h2>
+								<h3>Sorry! Page not found. <br><a href="{{URL::to('/')}}">Go to Home Page</a></h3>
+							</div>
+							<br>
+							<br>
+						</div>
+					</div>
 				</div>
-				<div class="clear"></div>
-			</div>
-
-			<div class="w3layouts-errortext">
-				<h2><span>4</span> <span><i class="fa fa-circle-o-notch" aria-hidden="true"></i></span> <span>4</span></h2>
-				<h3>Sorry! You do not have proper permission to visit this page. <br><a href="{{URL::to('/')}}">Go to Home Page</a></h3>
-
-			</div>
-		</div>
-	</div>
-	<!-- //main -->
-	<!-- copyright -->
-	<div class="copyright w3-agile">
-		<p>Copyright © 2019 Claim'N Win</p>
-	</div>
-	</div>
-	<!-- //copyright -->
-</body>
-</html>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
